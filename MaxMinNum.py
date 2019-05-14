ab15730bab68f2af5df9fb8121e26e51e126148b
Baqar Ali <baqarali789@gmail.com>
2019-05-14 18:33:53 +0500

    Another update

--- MaxMinNum.py
+++ MaxMinNum.py
@@ -12,18 +12,22 @@ max = arr[0]
 for i in range (1, ran):
     if arr[i] > max:
         max = arr[i]
+        index_max = i
 
 min = arr[0]
 for f in range (1, ran):
     if  min > arr[f]:
         min = arr[f]
+        index_min = f
 print(arr)
 
 strmax = str(max)
 strmin = str(min)
+IndexStr_max = str (index_max)
+IndexStr_min = str (index_min)
 
-print("Max value is : " + strmax)
-print("Min value is : " + strmin)
+print("Max value is : " + strmax + " Index is : " + IndexStr_max)
+print("Min value is : " + strmin + " Index is : " + IndexStr_min)
 
 val = arr[0]
 for k in range(1, ran):
